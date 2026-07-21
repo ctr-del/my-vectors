12b2a5f814e223747746cb8f6fb7cff4a4730e62
William <83tropic.storm@gmail.com>
2026-07-21 02:34:13 -0400

    Header Cleanup + Initial Access Operator Implementation

--- src/vectors.h
+++ src/vectors.h
@@ -2,6 +2,7 @@
 #define CUSTOM_VECTOR_HPP
 
 #include <cstddef>
+#include <stdexcept>
 
 template <typename T>                                                           //Aw heck, here we go again, welcome back Mr.GPT.
 class Vector {
@@ -10,17 +11,32 @@ class Vector {
         std::size_t _size;                                                      //This is the size of the vector, i.e. the part of the vector that actually has data.
         std::size_t _capacity;                                                  //This is the capacity of the vector, i.e. all the memory allocated to the vector at this point in time.
     public:
+        //Default vector constructor
         Vector();
+
+        //Vector deconstructor
         ~Vector();
 
-        void push_back(const T& value);
+        //Lets disable copying for now, but Vector Copy Constructor and Copy Assignment
+        Vector(const Vector&) = delete;
+        Vector& operator=(const Vector&) = delete;
 
+        //Move operations here for future implementation.
+        Vector(const Vector&&);
+        Vector&& operator=(const Vector&&);
+
+        //Access operators and methods
         T& operator[](std::size_t index);
         const T& operator[](std::size_t index) const;
+        T& at(std::size_t index);
+        const T& at(std::size_t index) const;
+
+        void push_back(const T& value);
 
         std::size_t size() const;
         std::size_t capacity() const;
         bool empty() const;
+
 };
 
 template <typename T>
@@ -38,6 +54,32 @@ Vector<T>::~Vector() {
 }
 
 template <typename T>
+T& Vector<T>::operator[](std::size_t index) {
+    return _data[index];
+}
+
+template <typename T>
+const T& Vector<T>::operator[](std::size_t index) const{
+    return _data[index];
+}
+
+template <typename T>
+T& Vector<T>::at(std::size_t index) {
+    if (index > _size) {
+        throw std::out_of_range_exception("Error! Attempted to access vector beyond the vector size.");
+    }
+    return *this[index];
+}
+
+template <typename T>
+const T& Vector<T>::at(std::size_t index) const {
+    if (index > _size) {
+        throw std::out_of_range_exception("Error! Attempted to access const vector boyond the vector size.");
+    }
+    return *this[index];
+}
+
+template <typename T>
 std::size_t Vector<T>::size() const {
     return _size;
 }
@@ -49,7 +91,7 @@ std::size_t Vector<T>::capacity() const {
 
 template <typename T>
 bool Vector<T>::empty() const {
-    return _data == nullptr;
+    return _size == 0;
 }
 
 #endif
