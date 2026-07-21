d0ac96e56bc25415a5e65b9e47124a32e9bd714e
William <83tropic.storm@gmail.com>
2026-07-21 01:28:30 -0400

    Constructor, Destructor, size(), and capacity() helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <cassert>
+
+#include "vectors.h"
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Test Suite A for my Vector implementation." << std::endl;
+
+    //Let's declare a Vector right here...
+    Vector<int> vector;
+    assert(vector.size() == 0);
+    assert(vector.capacity() == 0);
 }
---
+++ src/vectors.h
@@ -0,0 +1,55 @@
+#ifndef CUSTOM_VECTOR_HPP
+#define CUSTOM_VECTOR_HPP
+
+#include <cstddef>
+
+template <typename T>                                                           //Aw heck, here we go again, welcome back Mr.GPT.
+class Vector {
+    private:
+        T* _data;
+        std::size_t _size;                                                      //This is the size of the vector, i.e. the part of the vector that actually has data.
+        std::size_t _capacity;                                                  //This is the capacity of the vector, i.e. all the memory allocated to the vector at this point in time.
+    public:
+        Vector();
+        ~Vector();
+
+        void push_back(const T& value);
+
+        T& operator[](std::size_t index);
+        const T& operator[](std::size_t index) const;
+
+        std::size_t size() const;
+        std::size_t capacity() const;
+        bool empty() const;
+};
+
+template <typename T>
+Vector<T>::Vector() :
+    _data(nullptr),
+    _size(0),
+    _capacity(0)
+{
+
+}
+
+template <typename T>
+Vector<T>::~Vector() {
+    delete[] _data;
+}
+
+template <typename T>
+std::size_t Vector<T>::size() const {
+    return _size;
+}
+
+template <typename T>
+std::size_t Vector<T>::capacity() const {
+    return _capacity;
+}
+
+template <typename T>
+bool Vector<T>::empty() const {
+    return _size == 0;
+}
+
+#endif
