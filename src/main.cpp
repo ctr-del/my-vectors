f7db42509557a43e767f5de4d4fe926fbc2a030d
ctr-del <83tropic.storm@gmail.com>
2026-07-22 19:23:30 -0400

    Updated my tests to be more complete and descriptive

--- src/main.cpp
+++ src/main.cpp
@@ -42,12 +42,7 @@ namespace debug {
     }
 }
 
-void ok(){
-    std::cout << "Ok." << std::endl;
-}
-
 int main() {
-    #ifndef NDEBUG
     std::cout << "Vector Test Suite 2.0" << std::endl;
 
     //Vector Construction Tests...
@@ -89,6 +84,7 @@ int main() {
     });
 
     Test tVectorCopyConstructor("tVectorCopyConstructor", []() -> TestResult {
+        //Variable initialization...
         Vector<int> vector_a ({
             4,
             7,
@@ -100,22 +96,34 @@ int main() {
 
         Vector<int> vector_b (vector_a);
 
+        //Check to if the size and capacity carried over properly and if it didn't return a failed TestResult.
+        if(
+            vector_a.size() != vector_b.size() ||
+            vector_a.capacity() != vector_b.capacity()
+        ){
+            return TestResult(T_COPY_CONSTRUCT_MISMATCH,"Vector size or capacity mismatch indicated vector was not copied correctly.");
+        }
+        //Check to see if all the contents copied properly and if it didn't return a failed TestResult.
         for(std::size_t i = 0; i < vector_a.size(); i++){
             if(vector_a[i] != vector_b[i]){
-                return TestResult(T_COPY_CONSTRUCT_MISMATCH, "Vector copy constructor failed. Vector a does not have the same elements as vector b.");
+                return TestResult(T_COPY_CONSTRUCT_MISMATCH, std::format("Vector copy constructor failed. Vector a mismatch at index {}.", i));
             }
         }
 
+        //Change a value in vector_b
         vector_b[2] = 5;
 
+        //Make sure the value in vector_a didn't change as well because that means we have a shallow copy.
         if(vector_a[2] == vector_b[2]){
             return TestResult(T_COPY_CONSTRUCT_MISMATCH, "Vector copy constructor failed. Vector b is a shallow copy of vector b instead of a deep copy.");
         }
 
+        //Test is OK.
         return TestResult(T_SUCCESS);
     });
 
     Test tVectorCopyAssignment("tVectorCopyAssignment", []() -> TestResult {
+        //Initialize the vectors
         Vector<std::string> vector_a ({
             "gameboy",
             "gameboy advance",
@@ -123,12 +131,32 @@ int main() {
             "neogeo pocket"
         });
 
-        Vector<std::string> vector_b = vector_a;
+        Vector<std::string> vector_b = {};
 
-        vector_a.clear();
+        //Copy 'vector_a' to 'vector_b' overwriting it's previous values.
+        vector_b = vector_a;
 
-        if(vector_b.empty()){
-            return TestResult(T_COPY_ASSIGNMENT_MISMATCH, "Copy assignment failed. Copy of vector_a is not uniqe!");
+        //Check to if the size and capacity carried over properly and if it didn't return a failed TestResult.
+        if(
+            vector_a.size() != vector_b.size() ||
+            vector_a.capacity() != vector_b.capacity()
+        ){
+            return TestResult(T_COPY_ASSIGNMENT_MISMATCH,"Vector size or capacity mismatch indicated vector was not copied correctly.");
+        }
+
+        //Check to see if all the contents copied properly and if it didn't return a failed TEST_RESULT.
+        for(std::size_t i = 0; i < vector_a.size(); i++){
+            if(vector_a[i] != vector_b[i]){
+                return TestResult(T_COPY_ASSIGNMENT_MISMATCH, std::format("Vector copy constructor failed. Vector a mismatch at index {}.", i));
+            }
+        }
+
+        //Lets change one of the elements in vector_b.
+        vector_b[3] = "nintendo ds";
+
+        //And make sure it didn't effect vector_a as well and if it did, return an error.
+        if(vector_b[3] == vector_a[3]){
+            return TestResult(T_COPY_CONSTRUCT_MISMATCH, "Vector copy constructor failed. Vector b is a shallow copy of vector b instead of a deep copy.");
         }
 
         return TestResult(0);
@@ -138,167 +166,32 @@ int main() {
         Vector<int> vector_a ({3, 7, 5, 9 ,4});
         Vector<int> vector_b (std::move(vector_a));
 
-        if(vector_a.size() != 0) {
+        //Check to see if Vector A is empty.
+        if(!vector_a.empty()) {
             return TestResult(T_MOVE_CONSTRUCT_MISMATCH, "Vector A didn't get cleared. Move operation failed.");
         }
-        return TestResult(0);
-    });
 
-    Test tVectorMoveAssignment("tVectorMoveAssignment", []() -> TestResult {
-        Vector<int> vector_a ({3, 5, 7, 9, 5});
-        Vector<int> vector_b;
-        
-        vector_b = std::move(vector_a);
-
-        if(vector_a == vector_b){
-            return TestResult(T_MOVE_ASSIGNMENT_MISMATCH, "Vector A didn't get cleared. Move operation failed.");
+        //Check to see if Vector B is the appropriate size.
+        if(vector_b.size() != 5) {
+            return TestResult(T_MOVE_CONSTRUCT_MISMATCH, std::format("vector_b was supposed have a size of 5, but has a size of {}.", vector_b.size()));
         }
 
-        return TestResult(0);
+        //Check to make sure all the items made it over in the right order.
+        if(
+            vector_b[0] == 3 &&
+            vector_b[1] == 7 &&
+            vector_b[2] == 5 &&
+            vector_b[3] == 9 &&
+            vector_b[4] == 4
+        ){
+            //Test OK.
+            return TestResult(0);
+        }
+        
+        return TestResult(T_MOVE_CONSTRUCT_MISMATCH, "vector_b didn't match vector_a's elements after it was moved. Something went very wrong.");
     });
+
+
 
     Test::run_all();
-    #else
-    std::cout << "===[ Test Suite A for my Vector implementation. ]====================" << std::endl;
-
-    std::cout << "Declaring our Vector variable...";
-    Vector<std::string> vector;
-    ok();
-
-    std::cout << "Checking if the size is equal to zero...";
-    assert(vector.size() == 0);
-    ok();
-
-    std::cout << "Checking if the capacity is equal to zero...";
-    assert(vector.capacity() == 0);
-    ok();
-
-    std::cout << "Making sure that the vector is empty...";
-    assert(vector.empty() == true);
-    ok();
-
-
-    std::cout << "===[ Test Suit B for my Vector implementation. ]=====================" << std::endl;
-
-    std::cout << "Trying to add a few elements to the vector...";
-    vector.push_back("Mayonaise");
-    vector.push_back("Mustard");
-    vector.push_back("Ketchup");
-    vector.push_back("Brocolli");
-    vector.push_back("Tomato");
-    ok();
-    debug::print_vector(vector);
-
-    std::cout << "Checking to see if the size is equal to 5...";
-    assert(vector.size() == 5);
-    ok();
-
-    std::cout << "Checking to see if the capacity is equal to 8...";
-    assert(vector.capacity() == 8);
-    ok();
-
-    std::cout << "Checking [] operators: " << std::endl;
-    std::cout << "Element 0 is Mayonaise...";
-    assert(vector[0] == "Mayonaise");
-    ok();
-
-    std::cout << "Element 3 is Brocolli...";
-    assert(vector[3] == "Brocolli");
-    ok();
-
-    std::cout << "Testing popback()...";
-    vector.pop_back();
-    assert(vector.size() == 4);
-    ok();
-    debug::print_vector(vector);
-
-    std::cout << "Testing clear()...";
-    vector.clear();
-    assert(vector.empty());
-    ok();
-
-    std::cout << "Populating the vector again...";
-    vector.push_back("Pineapple");
-    vector.push_back("Lemon");
-    vector.push_back("Pear");
-    vector.push_back("Seabear");
-    vector.push_back("Tangerine");
-    vector.push_back("Salmon");
-    ok();
-    debug::print_vector(vector);
-
-    std::cout << "Testing reserve()...";
-    {
-        vector.reserve(64);
-        assert(vector.capacity() == 64);
-    }
-    ok();
-
-    std::cout << "Testing resize()...";
-    {
-        vector.resize(86);
-        assert(vector.capacity() == 86);
-        assert(vector.size() == 86);
-    }
-    ok();
-
-    std::cout << "Iterator testing" << std::endl;
-
-    std::cout << "Iterating through the Vector we already have..." << std::endl;
-    for (std::string element : vector){
-        std::cout << element << " ";
-    }
-    std::cout << std::endl;
-    ok();
-
-    {
-        std::cout << "Creating a const Vector to test const iterators...";
-        const Vector<int> const_vector ({3, 5, 17, 4, 9, 12, 3, 2, 19, 92, 5, 2, 9});
-        ok();
-
-        std::cout << "Iterating through the const Vector list we have..." << std::endl;
-        for (const auto element : const_vector) {
-            std::cout << element << " ";
-        }
-        ok();
-    }
-
-    {
-        std::cout << "Testing copy constructor and copy assignment..." << std::endl;
-        Vector<int> original ({3, 7, 5, 8, 12});
-        std::cout << "Trying to copy the original with a copy constructor..." << std::endl;
-        Vector<int> copyA (original);
-        std::cout << "Making a few changes to the copy to confirm it is indeed a copy..." << std::endl;
-        copyA[1] = 9;
-        copyA[3] = 2;
-        std::cout << "Comparing..." << std::endl;
-        debug::print_compare_vector(original, copyA);
-        std::cout << "Testing copy assignment..." << std::endl;
-        Vector<int> copyB ({102, 151, 522, 913, 210, 513, 512});
-        debug::print_compare_vector(copyB, original);
-        copyB = original;
-        debug::print_compare_vector(copyB, original);
-        copyB = copyA;
-        debug::print_compare_vector(copyB, original);
-    }
-
-    {
-        std::cout << "Testing move constructor and move assignment..." << std::endl;
-        Vector<int> original ({13, 57, 82, 13, 52});
-        debug::print_vector(original);
-        Vector<int> moved_vector (std::move(original));
-        debug::print_compare_vector(original, moved_vector);
-        std::cout << "Repopulating original list..." << std::endl;
-        original.push_back(12);
-        original.push_back(34);
-        original.push_back(26);
-        original.push_back(9);
-        debug::print_compare_vector(original, moved_vector);
-        std::cout << "Testing move assignment operator..." << std::endl;
-        moved_vector = std::move(original);
-        debug::print_compare_vector(original, moved_vector);
-    }
-
-    std::cout << "Looks like all tests have passed!" << std::endl;
-    #endif
 }
